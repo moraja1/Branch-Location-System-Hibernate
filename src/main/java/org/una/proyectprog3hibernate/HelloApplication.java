ade131111de56c885eddc6003bfe9f669fc4819b
moraja1 <jeisonmora0115@gmail.com>
2022-10-29 17:36:14 -0600

    proyect restarted

--- src/main/java/org/una/proyectprog3hibernate/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package org.una.proyectprog3hibernate;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
